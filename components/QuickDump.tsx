6326a1b7454233d957b74e50eb2895b5e118a949
Shijun Zhou <szhou92@jh.edu>
2026-02-25 21:29:59 +0900

    style: move quick dump button up to prevent overlap with global diary

--- components/QuickDump.tsx
+++ components/QuickDump.tsx
@@ -31,7 +31,7 @@ export function QuickDump() {
     }, [localContent, syncedContent, setSyncedContent])
 
     return (
-        <div className="fixed bottom-6 right-6 z-[100] flex flex-col items-end gap-3 pointer-events-none">
+        <div className="fixed bottom-24 right-6 z-[100] flex flex-col items-end gap-3 pointer-events-none">
             {/* Slide-out Panel */}
             <div
                 className={cn(
